93345f749939491dafc843739b816b4f7e8b8e22
NSKV00 <nicolas.g.g.dsilva@gmail.com>
2026-01-29 21:45:02 -0300

    exercícios

--- exercício2.py
+++ exercício2.py
@@ -1,5 +1,19 @@
 #2- Implemente um validador de senha: mínimo 8 caracteres, deve conter número e letra.
-senha = input("Digite sua senha: ")
 
-if len(senha) <= 8:
-    print("Senha inválida. Deve ter no mínimo 8 caracteres.")
+while True:
+    senha = input("Digite uma senha para validar (ou 'sair' para encerrar): ")
+    if senha.lower() == 'sair':
+        break
+
+    if len(senha) < 8:
+        print("Senha inválida. Deve ter no mínimo 8 caracteres.")
+    elif not any(char.isdigit() for char in senha):
+        print("Senha inválida. Deve conter pelo menos um número.")
+    elif not any(char.isalpha() for char in senha):
+        print("Senha inválida. Deve conter pelo menos uma letra.")
+    # elif not any(char.isupper() for char in senha):
+    #     print("Senha inválida. Deve conter pelo menos uma letra maiúscula.")
+    else:
+        print("Senha válida.")
+
+print("Validador de senha encerrado.")
